b906b8fcc561e6e75bbd6275021ce976f505faab
DA MAC M1 81 <damacm181@DA-Mac81s-MacBook-Air.local>
2023-09-08 13:29:54 +0200

    second work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { CouchsComponent } from './pages/couchs/couchs.component';
+import { HeadboardsComponent } from './pages/headboards/headboards.component';
+import { StoragesComponent } from './pages/storages/storages.component';
+import { BedsComponent } from './pages/beds/beds.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'couchs', component: LandingComponent },
+  { path: 'couchs', component: CouchsComponent },
+  { path: 'headboards', component: HeadboardsComponent },
+  { path: 'storages', component: StoragesComponent },
+  { path: 'beds', component: BedsComponent },
 ];
 
 @NgModule({
